0964b2879a3c355e9a663c20c6e51d3da22b265b
root <root@RootManWalking.localdomain>
2018-07-08 22:55:44 -0500

    iAdded more updates n stuffq

--- DemoAppsConfig.js
+++ DemoAppsConfig.js
@@ -1,6 +1,13 @@
+import {action} from './src/actions/store.js'
+
 export const DemoAppsConfig = [
 	{
 		appIcon: require('./src/assets/images/calc_icon.png'),
-		demoName: 'Calculator'
+		demoName: 'Calculator',
+    onPress: function() {
+      this.props.navigation.navigate('CalculatorAppScreen');
+      //action('LAUNCH_DEMO_APP_CALCULATOR')
+    },
+    key: 'calculator_demo_app'
 	},
 ];
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,59 @@
 import React from 'react';
-import {Platform, Text, View} from 'react-native';
+import {Text, View, ScrollView, StatusBar} from 'react-native';
+import {Provider} from 'react-redux';
+import {store} from '../actions/store.js'
 
+import LaunchDemoAppComponent from '../components/LaunchDemoAppComponent.js';
+import {DemoAppsConfig} from '../../DemoAppsConfig.js';
 
 export default class HomeScreen extends React.Component {
-
-	constructor() {
-		super(this);
-		
-	}
+  constructor(props) {
+    super(props);
+  }
 
   render() {
     return (
-      <View>
-        
+      <Provider store={store}>
+      <View style={{
+        flex: 1,
+        paddingTop: 20, //@TODO: Gonna need to figure the status bar hight 20 ios, 45 X, StatusBar.currentHeight android
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <View style={{
+         alignItems: 'center',
+         margin: 32
+        }}>
+          <Text style={{
+            fontSize: 24,
+            fontWeight: 'bold',
+            marginBottom: 24
+          }}>NateAGeek's Native Demos</Text>
+          <Text>
+            These are a series of demos that I am working on for React Native. You can view the source code on my GitHub.
+            Hope you enjoy these little demo apps and have fun with them 😊
+          </Text>
+        </View>
+        <ScrollView contentContainerStyle={{
+          justifyContent: 'center',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+        }}
+        style={{
+          flexGrow: 1,
+        }}>
+          {DemoAppsConfig.map((appConfig) => {
+            return(
+              <LaunchDemoAppComponent
+                demoName={appConfig.demoName}
+                appIcon={appConfig.appIcon}
+                onPress={appConfig.onPress.bind(this)}
+                key={appConfig.key}/>
+            );
+          })}
+        </ScrollView>
       </View>
+      </Provider>
     );
   }
 }
